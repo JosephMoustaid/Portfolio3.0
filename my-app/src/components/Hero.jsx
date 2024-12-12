d8fb5a52859922c017554e961dc2c4be5b6b7b8b
JosephMoustaid <youssefmoustaid90@gmail.com>
2024-12-12 14:00:45 +0100

    some

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
   }, []);
 
   return (
-    <section id="home" className="hero">
+    <section id="home" className="hero hhhhhh">
       <div className="container">
         <div className="row align-items-center">
           <div className="col-8" id="description">
